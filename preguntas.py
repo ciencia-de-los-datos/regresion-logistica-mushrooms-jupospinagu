0926b13c0cfd9918e58c9a35a2ebfd745c8e3de7
Juan Pablo Ospina Gutierrez <101611634+jupospinagu@users.noreply.github.com>
2022-05-12 13:01:51 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -122,7 +122,9 @@ def pregunta_03():
     # Importe LogisticRegressionCV
     # Importe OneHotEncoder
     # Importe Pipeline
-    from ____ import ____
+    from sklearn.preprocessing import OneHotEncoder
+     from sklearn.pipeline import Pipeline
+     from sklearn.linear_model import LogisticRegressionCV
 
     # Cargue las variables.
     X_train, _, y_train, _ = pregunta_02()
@@ -131,13 +133,13 @@ def pregunta_03():
     # LogisticRegression con una regularización Cs=10
     pipeline = Pipeline(
         steps=[
-            ("____", ____()),
-            ("____", ____(____)),
+            ("OHE", OneHotEncoder()),
+            ("RL", LogisticRegressionCV(Cs=10)),
         ],
     )
 
     # Entrene el pipeline con los datos de entrenamiento.
-    ____.____(____, ____)
+    pipeline.fit(X_train, y_train)
 
     # Retorne el pipeline entrenado
     return pipeline
